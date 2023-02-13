71710a853935b6a1aa93b618596a5b9db5b68ec4
dmoulinie <dan.moulinie@gmail.com>
2023-02-13 22:24:05 +1100

    Add project files.

---
+++ Hacks.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace ValheimHackGUI
+{
+	class Hacks : MonoBehaviour
+	{
+		public void OnGUI()
+		{
+			GUI.Label(new Rect(10f, 10f, 100f, 100f), "Woulah je veux du texte");
+		}
+
+		public void Start()
+		{
+
+		}
+
+		public void Update()
+		{
+			Player player= GetComponent<Player>();
+		}
+	}
+}
---
+++ Loader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace ValheimHackGUI
+{
+    public class Loader
+    {
+
+        public static void Init()
+        {
+            Loader.Load = new GameObject();
+            Loader.Load.AddComponent<Hacks>();
+            UnityEngine.Object.DontDestroyOnLoad(Loader.Load);
+        }
+
+        public static void Unload()
+        {
+            GameObject.Destroy(Load);
+        }
+        private static GameObject Load;
+    }
+}
